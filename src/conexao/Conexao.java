0e4ff046c950db7f48f9e2ee8cf6aa03663ef5ea
GABRIEL DOMINGOS BARBOSA DE BRITO RANGEL <gabriel.dbbrangel@senacsp.edu.br>
2024-10-02 15:39:24 -0300

    add arquivos

---
+++ src/conexao/Conexao.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+/**
+*
+* @author gabriel.dbbrangel
+*/
+public class Conexao {
+        public Connection getConexao(){
+    try{
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/sistema_energia",
+                "root",
+                "");
+        return conn;
+    }catch(Exception e){
+        System.out.println("Erro de conexão" + e.getMessage());
+        return null;
+    }
+} 
+}
